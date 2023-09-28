4b2d712eea13d50c076356934902f49428ad116c
Md Saber Hossain <mhmcb@missouri.edu>
2023-09-28 13:53:04 -0500

    breakdowns path updated

--- CDM_DATA/APP/SCRIPTS/OTHER/gpc/cleanup.sql
+++ CDM_DATA/APP/SCRIPTS/OTHER/gpc/cleanup.sql
@@ -13,3 +13,19 @@ where project_id = 'ACT';
 update I2B2HIVE.HIVE_CELL_PARAMS
 set value = '11'
 where param_name_cd = 'edu.harvard.i2b2.crc.setfinderquery.obfuscation.minimum.value';
+
+
+--gpc hide
+UPDATE i2b2data.QT_QUERY_RESULT_TYPE
+set VISUAL_ATTRIBUTE_TYPE_ID = 'LA'
+where name = 'PATIENT_GPCSITE_COUNT_XML';
+
+--visit details
+UPDATE i2b2data.QT_QUERY_RESULT_TYPE
+set classname = 'edu.harvard.i2b2.crc.dao.setfinder.QueryResultGenerator'
+where name = 'PATIENT_INOUT_XML';
+
+
+UPDATE i2b2data.QT_BREAKDOWN_PATH
+SET VALUE = '\\\\ACT_VISIT\\ACT\\Visit Details\\Visit type\\'
+WHERE NAME = 'PATIENT_INOUT_XML';
