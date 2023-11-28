6de4b8312018e88f7fadd156c2b3ebc8c29a413e
Md Saber Hossain <mhmcb@missouri.edu>
2023-11-28 10:58:36 -0600

    [Task]: Fact and dimension view implementation for MU and GPC pcornet to i2b2 mapping

---
+++ CDM_DATA/APP/SCRIPTS/DIMENSION_TABLES/gpc/02_visit_dimension.sql
@@ -0,0 +1,30 @@
+-- create view
+create or replace view #target_schema.VISIT_DIMENSION
+as  
+select
+    ec.ENCOUNTER_NUM                                                                                as ENCOUNTER_NUM, 
+    pc.PATIENT_NUM                                                                                  as PATIENT_NUM,
+    COALESCE(providerID, '@')                                                                       as PROVIDER_ID,
+    -- TO_TIMESTAMP(admit_date :: DATE || ' ' || admit_time, 'YYYY-MM-DD HH24:MI:SS')                  AS start_date,
+    -- TO_TIMESTAMP(COALESCE(discharge_date, admit_date) :: DATE || ' ' || COALESCE(discharge_time, '00:00:00'), 'YYYY-MM-DD HH24:MI:SS')      AS end_date,
+    admit_date :: TIMESTAMP                                                                         AS start_date,
+    discharge_date :: TIMESTAMP                                                                     AS end_date,
+    ENC_TYPE                                                                                        as INOUT_CD,
+    facilityid,
+    facility_location,
+    datediff(day, start_date, end_date)                                                             as length_of_stay,
+    cast(null as VARCHAR(50))                                                                       as ACTIVE_STATUS_CD,
+    PAYER_TYPE_PRIMARY,
+    cast(null as VARCHAR(50))                                                                       as LOCATION_CD,
+    cast(null as VARCHAR(900))                                                                      as LOCATION_PATH,
+    cast(null as text)                                                                              as VISIT_BLOB,
+    CURRENT_TIMESTAMP                                                                               as UPDATE_DATE,
+    CURRENT_TIMESTAMP                                                                               as DOWNLOAD_DATE,
+    CURRENT_TIMESTAMP                                                                               as IMPORT_DATE,
+    cast(null as VARCHAR(50))                                                                       as SOURCESYSTEM_CD,
+    cast(null as  integer)                                                                          as UPLOAD_ID
+from #source_schema.GPC_DEID_ENCOUNTER as dim   
+inner join #source_schema.patient_crosswalk as pc
+using (patid)
+inner join #source_schema.encounter_crosswalk as ec
+using (ENCOUNTERID); 
---
+++ CDM_DATA/APP/SCRIPTS/FACT_TABLES/mu/04_procedure_fact.sql
@@ -0,0 +1,30 @@
+create or replace view  #target_schema.PROCEDURE_FACT as
+select
+    cast(ENCOUNTERID as NUMBER(38, 0))                                                          as ENCOUNTER_NUM, 
+    cast(PATID as NUMBER(38, 0))                                                                as PATIENT_NUM, 
+    case 
+        when px_type = '10' then concat('ICD10PCS:',px)
+        when px_type = '09' then concat('ICD9PROC:',px)
+        when px_type = 'CH' then concat(raw_px_type,':', px)
+        else concat(px_type,':',px)      
+    end                                                                                         as CONCEPT_CD,
+    COALESCE(fact.PROVIDERID, '@')                                                              as PROVIDER_ID, 
+    ADMIT_DATE :: TIMESTAMP                                                                     as START_DATE,  
+    '@'                                                                                         as MODIFIER_CD,
+    1                                                                                           as INSTANCE_NUM, 
+    ''                                                                                          as VALTYPE_CD,
+    ''                                                                                          as TVAL_CHAR, 
+    cast(null as  integer)                                                                      as NVAL_NUM, 
+    ''                                                                                          as VALUEFLAG_CD,
+    cast(null as  integer)                                                                      as QUANTITY_NUM, 
+    '@'                                                                                         as UNITS_CD, 
+    cast(null  	as TIMESTAMP)                                                                   as END_DATE, 
+    '@'                                                                                         as LOCATION_CD, 
+    cast(null as  text)                                                                         as OBSERVATION_BLOB, 
+    cast(null as  integer)                                                                      as CONFIDENCE_NUM, 
+    CURRENT_TIMESTAMP                                                                           as UPDATE_DATE,
+    CURRENT_TIMESTAMP                                                                           as DOWNLOAD_DATE,
+    CURRENT_TIMESTAMP                                                                           as IMPORT_DATE,
+    cast(null as VARCHAR(50))                                                                   as SOURCESYSTEM_CD,                                                                    
+    cast(null as  integer)                                                                      as UPLOAD_ID
+from #source_schema.DEID_PROCEDURES fact;
